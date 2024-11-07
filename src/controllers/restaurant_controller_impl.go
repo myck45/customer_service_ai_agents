d78fb4d4626ddd82d68b0b1ca4ccfc6fdd924bba
dieg0code <diegoobando20@gmail.com>
2024-11-07 11:17:14 -0300

    refactor restaurant controller to improve error handling and response structure

--- src/controllers/restaurant_controller_impl.go
+++ src/controllers/restaurant_controller_impl.go
@@ -2,12 +2,12 @@ package controllers
 
 import (
 	"fmt"
+	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/proyectos01-a/RestaurantMenu/src/dtos"
 	"github.com/proyectos01-a/RestaurantMenu/src/dtos/request"
-	"github.com/proyectos01-a/RestaurantMenu/src/dtos/response"
 	"github.com/proyectos01-a/RestaurantMenu/src/services"
 	"github.com/sirupsen/logrus"
 )
@@ -16,219 +16,111 @@ type RestaurantControllerImpl struct {
 	restaurantService services.RestaurantService
 }
 
-// CreateRestaurant implements RestaurantController.
+func NewRestaurantControllerImpl(restaurantService services.RestaurantService) RestaurantController {
+	return &RestaurantControllerImpl{
+		restaurantService: restaurantService,
+	}
+}
+
 func (r *RestaurantControllerImpl) CreateRestaurant(c *gin.Context) {
 	createRestaurantReq := &request.CreateRestaurantReq{}
-	err := c.ShouldBindJSON(createRestaurantReq)
-	if err != nil {
-		logrus.WithError(err).Error("Error binding request")
-		res := dtos.BaseResponse[string]{
-			Code:   400,
-			Status: "Bad Request",
-			Msg:    fmt.Sprintf("Error binding request: %s", err.Error()),
-			Data:   "",
-		}
-
-		c.JSON(200, res)
+	if err := c.ShouldBindJSON(createRestaurantReq); err != nil {
+		r.handleError(c, http.StatusBadRequest, "Error binding request", err)
 		return
 	}
 
 	restaurant, err := r.restaurantService.CreateRestaurant(createRestaurantReq)
 	if err != nil {
-		logrus.WithError(err).Error("Error creating restaurant")
-		res := dtos.BaseResponse[string]{
-			Code:   500,
-			Status: "Internal Server Error",
-			Msg:    fmt.Sprintf("Error creating restaurant: %s", err.Error()),
-			Data:   "",
-		}
-
-		c.JSON(200, res)
+		r.handleError(c, http.StatusInternalServerError, "Error creating restaurant", err)
 		return
 	}
 
-	res := dtos.BaseResponse[uint]{
-		Code:   200,
-		Status: "OK",
-		Msg:    "Restaurant created successfully",
-		Data:   restaurant.ID,
-	}
-
-	c.JSON(200, res)
+	r.handleSuccess(c, http.StatusOK, "Restaurant created successfully", restaurant.ID)
 }
 
-// DeleteRestaurant implements RestaurantController.
 func (r *RestaurantControllerImpl) DeleteRestaurant(c *gin.Context) {
-
 	restaurantID := c.Param("id")
-
 	id, err := strconv.ParseUint(restaurantID, 10, 64)
 	if err != nil {
-		logrus.WithError(err).Error("Error parsing id")
-		res := dtos.BaseResponse[string]{
-			Code:   400,
-			Status: "Bad Request",
-			Msg:    fmt.Sprintf("Error parsing id: %s", err.Error()),
-			Data:   "",
-		}
-
-		c.JSON(200, res)
+		r.handleError(c, http.StatusBadRequest, "Error parsing id", err)
 		return
 	}
 
-	err = r.restaurantService.DeleteRestaurant(uint(id))
-	if err != nil {
-		logrus.WithError(err).Error("Error deleting restaurant")
-		res := dtos.BaseResponse[string]{
-			Code:   500,
-			Status: "Internal Server Error",
-			Msg:    fmt.Sprintf("Error deleting restaurant: %s", err.Error()),
-			Data:   "",
-		}
-
-		c.JSON(200, res)
+	if err := r.restaurantService.DeleteRestaurant(uint(id)); err != nil {
+		r.handleError(c, http.StatusInternalServerError, "Error deleting restaurant", err)
 		return
 	}
 
-	res := dtos.BaseResponse[string]{
-		Code:   200,
-		Status: "OK",
-		Msg:    "Restaurant deleted successfully",
-		Data:   "",
-	}
-
-	c.JSON(200, res)
+	r.handleSuccess(c, http.StatusOK, "Restaurant deleted successfully", nil)
 }
 
-// GetAllRestaurants implements RestaurantController.
 func (r *RestaurantControllerImpl) GetAllRestaurants(c *gin.Context) {
 	restaurants, err := r.restaurantService.GetAllRestaurants()
 	if err != nil {
-		logrus.WithError(err).Error("Error fetching restaurants")
-		res := dtos.BaseResponse[string]{
-			Code:   500,
-			Status: "Internal Server Error",
-			Msg:    fmt.Sprintf("Error fetching restaurants: %s", err.Error()),
-			Data:   "",
-		}
-
-		c.JSON(200, res)
+		r.handleError(c, http.StatusInternalServerError, "Error fetching restaurants", err)
 		return
 	}
 
-	res := dtos.BaseResponse[[]response.RestaurantResponse]{
-		Code:   200,
-		Status: "OK",
-		Msg:    "Restaurants fetched successfully",
-		Data:   restaurants.Restaurants,
-	}
-
-	c.JSON(200, res)
-
+	r.handleSuccess(c, http.StatusOK, "Restaurants fetched successfully", restaurants.Restaurants)
 }
 
-// GetRestaurantByID implements RestaurantController.
 func (r *RestaurantControllerImpl) GetRestaurantByID(c *gin.Context) {
 	restaurantID := c.Param("id")
-
 	id, err := strconv.ParseUint(restaurantID, 10, 64)
 	if err != nil {
-		logrus.WithError(err).Error("Error parsing id")
-		res := dtos.BaseResponse[string]{
-			Code:   400,
-			Status: "Bad Request",
-			Msg:    fmt.Sprintf("Error parsing id: %s", err.Error()),
-			Data:   "",
-		}
-
-		c.JSON(200, res)
+		r.handleError(c, http.StatusBadRequest, "Error parsing id", err)
 		return
 	}
 
 	restaurant, err := r.restaurantService.GetRestaurantByID(uint(id))
 	if err != nil {
-		logrus.WithError(err).Error("Error fetching restaurant")
-		res := dtos.BaseResponse[string]{
-			Code:   500,
-			Status: "Internal Server Error",
-			Msg:    fmt.Sprintf("Error fetching restaurant: %s", err.Error()),
-			Data:   "",
-		}
-
-		c.JSON(200, res)
+		r.handleError(c, http.StatusInternalServerError, "Error fetching restaurant", err)
 		return
 	}
 
-	res := dtos.BaseResponse[*response.RestaurantResponse]{
-		Code:   200,
-		Status: "OK",
-		Msg:    "Restaurant fetched successfully",
-		Data:   restaurant,
-	}
-
-	c.JSON(200, res)
+	r.handleSuccess(c, http.StatusOK, "Restaurant fetched successfully", restaurant)
 }
 
-// UpdateRestaurant implements RestaurantController.
 func (r *RestaurantControllerImpl) UpdateRestaurant(c *gin.Context) {
 	restaurantID := c.Param("id")
-
 	id, err := strconv.ParseUint(restaurantID, 10, 64)
 	if err != nil {
-		logrus.WithError(err).Error("Error parsing id")
-		res := dtos.BaseResponse[string]{
-			Code:   400,
-			Status: "Bad Request",
-			Msg:    fmt.Sprintf("Error parsing id: %s", err.Error()),
-			Data:   "",
-		}
-
-		c.JSON(200, res)
+		r.handleError(c, http.StatusBadRequest, "Error parsing id", err)
 		return
 	}
 
 	updateRestaurantReq := &request.UpdateRestaurantReq{}
-
-	err = c.ShouldBindJSON(updateRestaurantReq)
-	if err != nil {
-		logrus.WithError(err).Error("Error binding request")
-		res := dtos.BaseResponse[string]{
-			Code:   400,
-			Status: "Bad Request",
-			Msg:    fmt.Sprintf("Error binding request: %s", err.Error()),
-			Data:   "",
-		}
-
-		c.JSON(200, res)
+	if err := c.ShouldBindJSON(updateRestaurantReq); err != nil {
+		r.handleError(c, http.StatusBadRequest, "Error binding request", err)
 		return
 	}
 
 	restaurant, err := r.restaurantService.UpdateRestaurant(uint(id), updateRestaurantReq)
 	if err != nil {
-		logrus.WithError(err).Error("Error updating restaurant")
-		res := dtos.BaseResponse[string]{
-			Code:   500,
-			Status: "Internal Server Error",
-			Msg:    fmt.Sprintf("Error updating restaurant: %s", err.Error()),
-			Data:   "",
-		}
-
-		c.JSON(200, res)
+		r.handleError(c, http.StatusInternalServerError, "Error updating restaurant", err)
 		return
 	}
 
-	res := dtos.BaseResponse[*response.RestaurantResponse]{
-		Code:   200,
-		Status: "OK",
-		Msg:    "Restaurant updated successfully",
-		Data:   restaurant,
-	}
-
-	c.JSON(200, res)
+	r.handleSuccess(c, http.StatusOK, "Restaurant updated successfully", restaurant)
 }
 
-func NewRestaurantControllerImpl(restaurantService services.RestaurantService) RestaurantController {
-	return &RestaurantControllerImpl{
-		restaurantService: restaurantService,
+func (r *RestaurantControllerImpl) handleError(c *gin.Context, statusCode int, message string, err error) {
+	logrus.WithError(err).Error(message)
+	res := dtos.BaseResponse[string]{
+		Code:   statusCode,
+		Status: http.StatusText(statusCode),
+		Msg:    fmt.Sprintf("%s: %s", message, err.Error()),
+		Data:   "",
 	}
+	c.JSON(statusCode, res)
+}
+
+func (r *RestaurantControllerImpl) handleSuccess(c *gin.Context, statusCode int, message string, data interface{}) {
+	res := dtos.BaseResponse[interface{}]{
+		Code:   statusCode,
+		Status: http.StatusText(statusCode),
+		Msg:    message,
+		Data:   data,
+	}
+	c.JSON(statusCode, res)
 }
