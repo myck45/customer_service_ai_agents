ab5866015de9a1178ca066d600eb5cb13e840f07
dieg0code <diegoobando20@gmail.com>
2024-11-23 19:47:26 -0300

    fix: update API Gateway integration response status code to 200

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -794,7 +794,7 @@ resource "aws_api_gateway_integration_response" "bot_response_twilio_webhook_pos
   rest_api_id = aws_api_gateway_rest_api.restaurant_menu_api.id
   resource_id = aws_api_gateway_resource.bot_response_twilio_webhook.id
   http_method = aws_api_gateway_method.bot_response_twilio_webhook_post.http_method
-  status_code = aws_api_gateway_method_response.bot_response_twilio_webhook_post_200.status_code
+  status_code = "200"
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
