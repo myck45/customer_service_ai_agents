92874af72b1e96e969b3150f413bdba25462f26c
dieg0code <diegoobando20@gmail.com>
2024-12-05 12:18:26 -0300

    fix: implement singleton pattern for database connection initialization

--- shared/config/database.go
+++ shared/config/database.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"sync"
 
 	"github.com/proyectos01-a/shared/models"
 	"github.com/sirupsen/logrus"
@@ -11,46 +12,51 @@ import (
 	"gorm.io/gorm"
 )
 
+var (
+	db   *gorm.DB
+	once sync.Once
+)
+
 func DatabaseConnection() *gorm.DB {
-	host := os.Getenv("DB_HOST")
-	port, _ := strconv.Atoi(os.Getenv("DB_PORT"))
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
+	once.Do(func() {
+		host := os.Getenv("DB_HOST")
+		port, _ := strconv.Atoi(os.Getenv("DB_PORT"))
+		user := os.Getenv("DB_USER")
+		password := os.Getenv("DB_PASSWORD")
+		dbname := os.Getenv("DB_NAME")
 
-	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require",
-		host, port, user, password, dbname)
+		sqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require",
+			host, port, user, password, dbname)
 
-	gormConfig := &gorm.Config{
-		PrepareStmt: true,
-	}
+		gormConfig := &gorm.Config{
+			PrepareStmt: true,
+		}
 
-	db, err := gorm.Open(postgres.Open(sqlInfo), gormConfig)
-	if err != nil {
-		logrus.Fatalf("Error connecting to database: %v", err)
-		panic("Failed to connect to database")
-	}
+		var err error
+		db, err = gorm.Open(postgres.Open(sqlInfo), gormConfig)
+		if err != nil {
+			logrus.Fatalf("Error connecting to database: %v", err)
+			panic("Failed to connect to database")
+		}
 
-	err = db.Exec("CREATE EXTENSION IF NOT EXISTS vector").Error
-	if err != nil {
-		logrus.Fatalf("Error enabling vector extension: %v", err)
-		panic("Failed to enable vector extension")
-	}
+		if err = db.Exec("CREATE EXTENSION IF NOT EXISTS vector").Error; err != nil {
+			logrus.Fatalf("Error enabling vector extension: %v", err)
+			panic("Failed to enable vector extension")
+		}
 
-	err = db.AutoMigrate(
-		&models.User{},
-		&models.Restaurant{},
-		&models.Bot{},
-		&models.Menu{},
-		&models.ChatHistory{},
-		&models.MenuFile{},
-		&models.UserOrder{},
-		&models.OrderMenuItem{},
-	)
-
-	if err != nil {
-		logrus.Warnf("Error migrating models: %v", err)
-	}
+		if err = db.AutoMigrate(
+			&models.User{},
+			&models.Restaurant{},
+			&models.Bot{},
+			&models.Menu{},
+			&models.ChatHistory{},
+			&models.MenuFile{},
+			&models.UserOrder{},
+			&models.OrderMenuItem{},
+		); err != nil {
+			logrus.Warnf("Error migrating models: %v", err)
+		}
+	})
 
 	return db
 }
