2338d9048a888b6a87e5dd535a3930bca37106f6
dieg0code <diegoobando20@gmail.com>
2024-12-05 12:31:50 -0300

    fix: increase varchar length for phone numbers, payment method, and status in UserOrder model

--- shared/models/user_order.go
+++ shared/models/user_order.go
@@ -10,12 +10,12 @@ type UserOrder struct {
 	OrderCode       uuid.UUID       `gorm:"type:uuid;not null;unique"`
 	DeliveryAddress string          `gorm:"type:varchar(255);not null"`
 	UserName        string          `gorm:"type:varchar(100);not null"`
-	PhoneNumber     string          `gorm:"type:varchar(20);not null"`
-	PaymentMethod   string          `gorm:"type:varchar(20);not null"`
+	PhoneNumber     string          `gorm:"type:varchar(30);not null"`
+	PaymentMethod   string          `gorm:"type:varchar(30);not null"`
 	TotalPrice      int             `gorm:"not null"`
-	BotWspNumber    string          `gorm:"type:varchar(20);not null"`
-	SenderWspNumber string          `gorm:"type:varchar(20);not null"`
-	Status          string          `gorm:"type:varchar(20);not null"`
+	BotWspNumber    string          `gorm:"type:varchar(30);not null"`
+	SenderWspNumber string          `gorm:"type:varchar(30);not null"`
+	Status          string          `gorm:"type:varchar(30);not null"`
 	RestaurantID    uint            `gorm:"not null;index"`
 	Restaurant      Restaurant      `gorm:"foreignKey:RestaurantID"`
 	OrderMenuItems  []OrderMenuItem `gorm:"foreignKey:UserOrderID"`
